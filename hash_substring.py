b1824f1f01b531a02a81b28ef72cedb0b02ea16b
Matiss <matiss.laksevics12@gmail.com>
2023-04-14 20:53:14 +0300

    update

--- hash_substring.py
+++ hash_substring.py
@@ -2,7 +2,21 @@
 
 # Matīss Lakševics 4.grupa, DITF, IT
 def read_input():
-    return (input().rstrip(), input().rstrip())
+    text = input()
+    if "I" in text:
+       input_p = input()
+       input_t = input()
+    if "F" in text:
+        filename = input()
+        filename = "tests/" + filename
+        if 'a' not in filename:
+            try:
+                with open(filename, "r") as file:
+                    input_p = file.readline()
+                    input_t = file.readline()
+            except:
+                return "File not found"
+    return (input_p.rstrip(), input_t.rstrip())
 def print_occurrences(output):
     print(' '.join(map(str, output)))
 def get_occurrences(pattern, text):
@@ -19,6 +33,6 @@ def hash(s, p, x):
     h = 0
     for c in reversed(s):
         h = (h * x + ord(c)) % p
-    return h
+    #return h
 if __name__ == '__main__':
     print_occurrences(get_occurrences(*read_input()))
